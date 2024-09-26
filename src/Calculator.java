fa1f78b07f7de70d1f8765464e622f89aee0370e
niklas.scharfy <nscharfy@gmail.com>
2024-09-26 16:22:21 +0200

    wfwijfiwof

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,22 @@
 public class Calculator {
+
+    /**
+     * Main Methode zum Starten des Programms
+     * @param args
+     */
     public static void main(String[] args) {
         int add1 = 5;
         int add2 = 3;
 
         int sum = add1 + add2;
-
         String calculatorName = "Casio";
         System.out.println("Sum: " + sum);
         System.out.println("Mein Rechner Name : " + calculatorName);
+
+        // Ich bin ein Kommentar
+
+        /*
+        Ich bin ein Kommentar
+         */
     }
 }
