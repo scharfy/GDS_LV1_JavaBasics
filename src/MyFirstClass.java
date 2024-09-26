6c29a9b222bb055d57d3c8e11df811beb676d59b
niklas.scharfy <nscharfy@gmail.com>
2024-09-26 14:47:19 +0200

    Erste Klasse erstellt

---
+++ src/MyFirstClass.java
@@ -0,0 +1,13 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+            System.out.println("Gulasch");
+        }
+
+        // Hier endet mein Code
+
+    }
+}
