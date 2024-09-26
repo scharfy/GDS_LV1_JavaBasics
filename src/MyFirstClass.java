a61d8949fc798a1c5e5f573efb0720bbe2f3b27f
niklas.scharfy <nscharfy@gmail.com>
2024-09-26 15:24:30 +0200

    init VariablesDemo

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -10,4 +10,38 @@ public class MyFirstClass {
         // Hier endet mein Code
 
     }
+
+    public void myMethod() {
+        System.out.println("This is my method");
+    }
+
+    public void myMethod2() {
+        System.out.println("This is my method 2");
+    }
+
+    public void myMethod3() {
+        System.out.println("This is my method 3");
+    }
+
+    public void myMethod4() {
+        System.out.println("This is my method 4");
+    }
+
+    public void myMethod5() {
+        System.out.println("This is my method 5");
+    }
+
+    public void myMethod6() {
+        System.out.println("This is my method 6");
+    }
+
+    public void myMethod7() {
+        System.out.println("This is my method 7");
+    }
+
+    public void myMethod8() {
+        System.out.println("This is my method 8");
+    }
+
+
 }
---
+++ src/VariablesDemo.java
@@ -0,0 +1,34 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int a;
+
+        a = 100;
+
+        System.out.println(a);
+
+        long z = 200;
+        long y;
+        y = a;
+        System.out.println(y);
+
+        // long to int nicht möglich
+        //int b = y;
+
+        // Deklaration und Initialisierung
+        int c;
+        int e,f,g;
+        int j,l = 100;
+        String name;
+
+        c = 2;
+        e = 3;
+        f = 4;
+        g = 5;
+        name = "Maxi Mustermann";
+
+        System.out.println(e);
+        System.out.println("c = " + c);
+        System.out.println("name = " + name); //soutv automatische ausgabe von variablen
+
+    }
+}
