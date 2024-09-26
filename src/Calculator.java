153b987218f6609ac2de7294026b45aecb494e7b
niklas.scharfy <nscharfy@gmail.com>
2024-09-26 16:05:50 +0200

    init Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int add1 = 5;
+        int add2 = 3;
+
+        int sum = add1 + add2;
+
+        String calculatorName = "Casio";
+        System.out.println("Sum: " + sum);
+        System.out.println("Mein Rechner Name : " + calculatorName);
+    }
+}
